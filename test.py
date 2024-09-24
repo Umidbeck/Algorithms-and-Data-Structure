aeb3e01ba4765991b42af190ba8f238fdab998ff
Umidjon <79796851+Umidbeck@users.noreply.github.com>
2024-09-24 18:57:28 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -229,5 +229,43 @@ def bubbleSort(array: list):
                 
                 
                 
-array = [32,53,22,55,432,31]
-bubbleSort(array)
+#array = [32,53,22,55,432,31]
+#bubbleSort(array)
+
+
+# Graphga moisollar
+
+from collections import deque
+
+def search(start_node, target='elon musk'):
+    search_queue = deque()
+    search_queue += graph[start_node]
+    searched = set()
+    
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            print(person)
+            if person == target:
+                print(f"{target}, topildi")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.add(person) 
+                
+    return False
+
+graph = {'siz': ['ali', 'vali', 'tohir'],
+             'ali': ['aziza', 'olim'],
+             'vali': ['botir', 'ziyoda'],
+             'tohir': ['elon musk', 'mohir'],
+             'olim': [],
+             'aziza': [],
+             'botir': [],
+             'ziyoda': ['aziza'],
+             'elon musk': [],
+             'mohir': []
+             }
+
+print(search('siz', 'ziyoda'))
+            
